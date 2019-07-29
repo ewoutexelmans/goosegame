88d6238d8ec6b2fa5fde6a3629c23eb40e601ac4
Ewout Exelmans <ewout.exelmans@outlook.com>
2019-07-29 09:37:57 +0200

    file new project

---
+++ GooseGame/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GooseGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("hello world");
+            Console.ReadKey();
+        }
+    }
+}
